446d68d9e00ee7ad62dd4b2e0aea85d6cc0d0034
sayonara312 <phantrong321@gmail.com>
2022-06-10 19:26:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import Content from "./Content.js";
 
+import ToDoList from './ToDoList.js'
 function App() {
-  const [show, setShow] = useState(false);
+  // const [show, setShow] = useState(false);
 
   return (
-    <div className="container">
-      <button
-        className=" mt-3 btn btn-primary"
-        onClick={() => {
-          setShow(!show);
-        }}
-      >
-        Toggle
-      </button>
-      {show && <Content />}
-    </div>
+    // <div className="container">
+    //   <button
+    //     className=" mt-3 btn btn-primary"
+    //     onClick={() => {
+    //       setShow(!show);
+    //     }}
+    //   >
+    //     Toggle
+    //   </button>
+    //   {show && <Content />}
+    // </div>
+    <ToDoList/>
   );
 }
 
